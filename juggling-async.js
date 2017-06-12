6f8c31c235c1eb7a2c7b336dae80531a9bfe7349
raj454raj <raj454raj@gmail.com>
2017-06-12 11:26:43 +0530

    cleaner juggling

--- juggling-async.js
+++ juggling-async.js
@@ -1,19 +1,24 @@
 var http = require('http');
 var bl = require('bl');
+var results = [null, null, null], count = 0;
 
-// Very very ugly code
-http.get(process.argv[2], function(response) {
-    response.pipe(bl(function (err, data1) {
-        http.get(process.argv[3], function(response) {
-            response.pipe(bl(function (err, data2) {
-                http.get(process.argv[4], function(response) {
-                    response.pipe(bl(function (err, data3) {
-                        console.log(data1.toString());
-                        console.log(data2.toString());
-                        console.log(data3.toString());
-                    }));
-                });
-            }));
-        });
-    }));
-});
+function printContents() {
+    count++;
+    if (count != 3) return;
+    for (var i = 0 ; i < 3 ; ++i) {
+        console.log(results[i]);
+    }
+}
+
+function getRequest(index) {
+    http.get(process.argv[index], function (response) {
+        response.pipe(bl(function(err, data) {
+            results[index - 2] = data.toString();
+            printContents();
+        }));
+    });
+}
+
+for (var i = 2 ; i < 5 ; ++i) {
+    getRequest(i);
+}
