c7d20098ea8e51e0629a2f6d4fb3c81fc1d53fa9
raj454raj <raj454raj@gmail.com>
2017-06-12 18:40:46 +0530

    sliding window optimization

--- assignment/new-server.js
+++ assignment/new-server.js
@@ -1,21 +1,20 @@
 #!/usr/bin/env node
+"use strict";
+
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 var fs = require("fs");
 var port = 8000;
 var filePath = process.argv[3];
+var currentWindow = [];
 
 var server = http.createServer(function(request, response) {
     if (request.url === "/log") {
         var WebSocketClient = require('websocket').client;
         var client = new WebSocketClient();
         client.on('connect', function(connection) {
-            console.log('WebSocket Client Connected');
             connection.on('error', function(error) {
                 console.log("Connection Error: " + error.toString());
-            });
-            connection.on('close', function() {
-                console.log('echo-protocol Connection Closed');
             });
             connection.on('message', function(message) {
                 response.end(message.utf8Data);
@@ -34,26 +33,34 @@ var server = http.createServer(function(request, response) {
 
 server.listen(port);
 
-wsServer = new WebSocketServer({httpServer: server});
+var wsServer = new WebSocketServer({httpServer: server});
 
 wsServer.on('request', function(request) {
     var connection = request.accept('tailfsocket', request.origin);
-        /* Show initial contents of file on first request */
-    fs.readFile(filePath, function (err, data) {
-        if (err) throw err;
-        connection.sendUTF(data);
-        var fd = fs.openSync(filePath, "r");
-        fs.watchFile(filePath,
-                     {persistent: true, interval: 100},
-                     function (curr, prev) {
-            var buffer = new Buffer(" ".repeat(10000));
-            fs.read(fd, buffer, 0,
-                    curr.size - prev.size + 1,
-                    prev.size,
-                    function (err, bytesRead, buffer) {
-                connection.sendUTF(buffer.toString().trim() + "\n");
-                buffer = new Buffer(" ".repeat(10000));
-            });
+    /* Show last 10 lines of the file on first request */
+    if (currentWindow.length < 10) {
+        fs.readFile(filePath, function (err, data) {
+            if (err) throw err;
+            currentWindow = data.toString().split("\n").slice(-11, -1);
+            connection.sendUTF(currentWindow.join("\n") + "\n");
+        });
+    } else {
+        connection.sendUTF(currentWindow.join("\n") + "\n");
+    }
+
+    var fd = fs.openSync(filePath, "r");
+    fs.watchFile(filePath,
+                 {persistent: true, interval: 100},
+                 function (curr, prev) {
+        var buffer = new Buffer(" ".repeat(10000));
+        fs.read(fd, buffer, 0,
+                curr.size - prev.size + 1,
+                prev.size,
+                function (err, bytesRead, buffer) {
+            currentWindow.push.apply(currentWindow, buffer.toString().trim().split("\n"));
+            currentWindow = currentWindow.slice(-10);
+            connection.sendUTF(buffer.toString().trim() + "\n");
+            buffer = new Buffer(" ".repeat(10000));
         });
     });
 });
